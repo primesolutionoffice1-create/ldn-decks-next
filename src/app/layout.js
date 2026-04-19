d1261ee5a8dc314665c632dfb8b6e19aad13ddb3
primesolutionoffice1-create <primesolution.office1@gmail.com>
2026-04-19 15:49:57 -0400

    feat: add GTM-N87MG6QS script to layout - dataLayer init + noscript fallback

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,39 @@
 import { Outfit } from "next/font/google";
 import "./variables.css";
 import "./globals.css";
+import Script from "next/script";
 import StructuredData from "../components/StructuredData";
 
 const outfit = Outfit({
-    variable: "--font-outfit",
-    subsets: ["latin"],
+  variable: "--font-outfit",
+  subsets: ["latin"],
 });
 
 import { buildMetadata, SITE_URL } from "@/lib/seo";
 
 export const metadata = buildMetadata({
-    path: "/",
-    title: "Deck Builder in Loudoun County | Custom Decks | LDN Decks",
-    description: "Top-rated deck builder in Loudoun County, VA. Trex Platinum Partner & TimberTech Certified. Custom composite & wood decks from $15k+. Serving Ashburn, Leesburg & all of Northern VA. Free estimate — call (571) 655-7207.",
-    image: "/images/img64.jpeg",
+  path: "/",
+  title: "Deck Builder in Loudoun County | Custom Decks | LDN Decks",
+  description: "Top-rated deck builder in Loudoun County, VA. Trex Platinum Partner & TimberTech Certified. Custom composite & wood decks from $15k+. Serving Ashburn, Leesburg & all of Northern VA. Free estimate — call (571) 655-7207.",
+  image: "/images/img64.jpeg",
 });
 
 // Global metadata overrides
 metadata.metadataBase = new URL(SITE_URL);
 metadata.title = {
   default: "Deck Builder in Loudoun County | Custom Decks | LDN Decks",
-    template: "%s | LDN Decks",
+  template: "%s | LDN Decks",
 };
 metadata.icons = {
-    icon: "/ldndecks-logo.webp",
-    apple: "/ldndecks-logo.webp",
+  icon: "/ldndecks-logo.webp",
+  apple: "/ldndecks-logo.webp",
 };
 metadata.other = {
-    "geo.region": "US-VA",
-    "geo.placename": "Loudoun County, Virginia",
-    "geo.position": "38.8404;-77.4289",
-    "ICBM": "38.8404, -77.4289",
+  "geo.region": "US-VA",
+  "geo.placename": "Loudoun County, Virginia",
+  "geo.position": "38.8404;-77.4289",
+  "ICBM": "38.8404, -77.4289",
 };
-
 metadata.verification = {
   google: "KqDI0PPrY8iiZYZI-hk1ikIKVqCtIcTrO3dHSM7U-Eg",
 };
@@ -42,21 +42,46 @@ import { ContactProvider } from "@/context/ContactContext";
 import LayoutContent from "./LayoutContent";
 
 export default function RootLayout({ children }) {
-    return (
-          <html lang="en" className={outfit.variable}>
+  return (
+    <html lang="en" className={outfit.variable}>
       <head>
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Google Tag Manager - dataLayer init */}
+        <Script id="gtm-init" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];`}
+        </Script>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N87MG6QS');`,
+          }}
+        />
       </head>
-        <body>
-              <ContactProvider>
-                <a href="#main" className="skip-link">Skip to main content</a>
-            <StructuredData />
-                <LayoutContent>
-    {children}
-      </LayoutContent>
-      </ContactProvider>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N87MG6QS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <ContactProvider>
+          <a href="#main" className="skip-link">Skip to main content</a>
+          <StructuredData />
+          <LayoutContent>
+            {children}
+          </LayoutContent>
+        </ContactProvider>
       </body>
-      </html>
-    );
+    </html>
+  );
 }
